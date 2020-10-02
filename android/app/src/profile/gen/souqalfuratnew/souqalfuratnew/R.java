bf1be7e9382ed31f3b5baba93002bc2b8d7fb879
khaled <khaled_salehalali@hotmail.com>
2020-10-02 13:30:00 +0300

    اضافة المحافظات

---
+++ android/app/src/profile/gen/souqalfuratnew/souqalfuratnew/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package souqalfuratnew.souqalfuratnew;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
